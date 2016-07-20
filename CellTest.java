12dc33d0fd0892c03cf6e53ad92265f88a6a0aa2
Hulkghl <hulkghl@gmail.com>
2016-07-20 00:06:37 -0400

    Initial Commit

---
+++ CellTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/**
+ * 
+ * @author Haolin Guo
+ * Deliverable 5 - Performance Testing - Pinning tests for the modification 1 
+ *
+ */
+
+
+public class CellTest 
+{
+	// Test 1: Pinning test to ensure that the modified method "toString" returns the expected value with an alive.
+	@Test
+	public void CellTest1() 
+	{
+		Cell c = new Cell();
+		c.setAlive(true);
+		assertTrue(c.toString().equals("X"));
+	}
+	
+	// Test 2: Pinning test to ensure that the modified method "toString" returns the expected value with a dead.
+	@Test
+	public void CellTest2() 
+	{
+		Cell c = new Cell();
+		c.setAlive(false);
+		assertTrue(c.toString().equals("."));
+	}
+	
+	// Test 3: Pinning test to ensure that the modified method "toString" returns the expected value with a cell that is dead now,
+	// but were once alive during the previous run.
+	@Test
+	public void CellTest3() 
+	{
+		Cell c = new Cell();
+		c.setAlive(true);
+		c.setAlive(false);
+		assertTrue(c.toString().equals("."));
+	}
+
+}
